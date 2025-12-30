dea59bd039ec30c7ae09e7c6b379472555dcbd45
piyush rai <piyushrai7271@gmail.com>
2025-12-30 18:44:34 +0530

    linear search question

--- Arrays/1.linear_search.js
+++ Arrays/1.linear_search.js
@@ -53,27 +53,81 @@
 // })(arr,target);
 
 // Find the first occurrence and last occurrence of a number.
-let arr = [1,2,3,4,5,6,7,1,2,3,1,2,3];
-let target = 3;
-let firstOccurance = null;
-let lastOccutance = null;
-let count =0;
+// let arr = [1,2,3,4,5,6,7,1,2,3,1,2,3];
+// let target = 3;
+// let firstOccurance = null;
+// let lastOccutance = null;
+// let count =0;
 
-for(let i=0; i<arr.length ; i++){
+// for(let i=0; i<arr.length ; i++){
 
-    if(arr[i] === target && count === 0){
-        firstOccurance = i;
-        lastOccutance = i;
-        count++;
-    }else if(arr[i] === target){
-        lastOccutance = i;
-    }
+//     if(arr[i] === target && count === 0){
+//         firstOccurance = i;
+//         lastOccutance = i;
+//         count++;
+//     }else if(arr[i] === target){
+//         lastOccutance = i;
+//     }
 
-}
-console.log(`first occurance is : ${firstOccurance} and lastOccurence is : ${lastOccutance}`)
-
-
-// function first_last_occurence (arr){
-    
 // }
-// first_last_occurence(arr);
+// console.log(`first occurance is : ${firstOccurance} and lastOccurence is : ${lastOccutance}`)
+
+// Find the first occurrence and last occurrence of a number.
+//  let arr = [1,2,3,4,5,6,7,1,2,3,1,2,3];
+//  let target = 3;
+// let firstOccurance = null;
+// let lastOccutance = null;
+
+// const findFirst_lastOccurance = function (arr,target){
+   
+//     if(arr[i] === target && firstOccurance !== null){
+//         firstOccurance = i;
+//         lastOccutance = i;
+//     }else if(arr[i] === target ){
+//         lastOccutance = i
+//     }
+//     return [lastOccutance,firstOccurance];
+// }
+// findFirst_lastOccurance(arr,target);
+
+// new day.....................
+//Check if the array is sorted (ascending order)
+// let arr = [1,2,3,4,5,6,7,8,9,10];
+
+// const isSorted = function (arr) {
+//     let lastElement = arr[0];
+//     let isascending = true;
+//     for(let i=0; i<arr.length; i++){
+//          if(lastElement > arr[i]){
+//             isascending = false;
+//             break;
+//          }
+//          lastElement = arr[i];
+//     }
+//     return isascending;
+// }
+// console.log(isSorted(arr));
+
+// Find the first repeating element in an array
+let arr = [1, 2, 3, 4, 2, 1, 3];
+
+let minSecondIndex = arr.length;
+let result = -1;
+
+for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+        if (arr[i] === arr[j]) {
+            if (j < minSecondIndex) {
+                minSecondIndex = j;
+                result = arr[i];
+            }
+            break; // stop after second occurrence
+        }
+    }
+}
+
+console.log(result);
+
+
+
+
