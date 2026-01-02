a56879f759b967d9240344e9b3686b425d4ee07d
piyush rai <piyushrai7271@gmail.com>
2026-01-02 19:48:31 +0530

    question on sorting

---
+++ Arrays/3.sorting_technique.js
@@ -0,0 +1,21 @@
+// question on sorting technique like {Bubble sort and Selection sort}
+
+//Sort an array in ascending order using Bubble Sort
+//  [5, 3, 8, 4, 2]
+// let arr =  [5, 3, 8, 4, 2];
+
+// const bubbleSort = function (arr){
+   
+//     for(let i=0; i<arr.length-1; i++){
+
+//         for(let j=0; j<arr.length-1-i; j++){
+//             if(arr[j] > arr[j+1]){
+//                 let temp = arr[j+1];
+//                 arr[j+1] = arr[j];
+//                 arr[j] = temp;
+//             }
+//         }
+//     }
+// }
+// bubbleSort(arr);
+// console.log(arr)
