304c64f3aad4bb7177a6b65e7c1940d028f791b0
piyush rai <piyushrai7271@gmail.com>
2026-02-09 12:34:13 +0530

    floyed algo problem solving

--- linked list/3.cyclic_linkedList.js
+++ linked list/3.cyclic_linkedList.js
@@ -185,3 +185,107 @@
 // // list.deleteNodeByValue(90);
 // // console.log("Is element exist in list :",list.search(1));
 // list.traverse()
+
+// // creating node
+// function Node (value, next = null){
+//     this.value = value;
+//     this.next = next;
+// }
+
+// // create linkedList
+// function linkedList (){
+//     this.head = null;
+//     this.length = 0;
+// }
+
+// linkedList.prototype.addItem = function (value){
+//     let newNode = new Node(value);
+//     if(this.head === null){
+//         this.head = newNode;
+//     }else{
+//         let currentNode = this.head;
+//         while(currentNode.next !== null){
+//             currentNode = currentNode.next;
+//         }
+//         currentNode.next = newNode;
+//     }
+//     this.length++;
+// }
+
+// // checking that is linked list is cyclic
+// linkedList.prototype.hasCycle = function(){
+//     if(this.head === null || this.head.next === null){
+//         return false;
+//     }
+
+//     let slow = this.head;
+//     let fast = this.head;
+//     while(fast !== null && fast !== slow){
+//         slow = slow.next;
+//         fast = fast.next.next;
+//     }
+
+//     if(fast === null) return false;
+
+//     if(fast === slow) return true;
+// }
+
+// // find cyclic start
+// linkedList.prototype.findCycleStart = function () {
+//     if (this.head === null) return null;
+
+//     let slow = this.head;
+//     let fast = this.head;
+
+//     // Step 1: detect cycle
+//     while (fast !== null && fast.next !== null) {
+//         slow = slow.next;
+//         fast = fast.next.next;
+
+//         if (slow === fast) {
+//             // Step 2: find cycle start
+//             let pointer1 = this.head;
+//             let pointer2 = slow;
+
+//             while (pointer1 !== pointer2) {
+//                 pointer1 = pointer1.next;
+//                 pointer2 = pointer2.next;
+//             }
+
+//             return pointer1.value; // cycle start
+//         }
+//     }
+
+//     return null; // no cycle
+// };
+
+
+// //travers the node
+// linkedList.prototype.traverse = function (){
+//     if(this.head === null){
+//         return null;
+//     }else{
+//         let currentNode = this.head;
+//         while(currentNode !== null){
+//             console.log(currentNode.value);
+//             currentNode = currentNode.next;
+//         }
+//     }
+// }
+
+
+
+// let list = new linkedList();
+// list.addItem(10);
+// list.addItem(20);
+// list.addItem(30);
+// list.addItem(40);
+// list.addItem(50);
+// list.addItem(60);
+// list.addItem(70);
+// list.addItem(80);
+// list.addItem(90);
+// list.addItem(100);
+// // list.hasCycle();
+// list.findCycleStart()
+// list.traverse()
--- linked list/4.mixed_linkedlist.js
+++ linked list/4.mixed_linkedlist.js
@@ -1,135 +1,240 @@
 // mixed question of linked list
 
-// create node
-function Node (value, next = null){
+// // create node
+// function Node (value, next = null){
+//     this.value = value;
+//     this.next = next;
+// }
+
+// // create linkedList
+// function linkedList (){
+//     this.head = null;
+//     this.length = 0;
+// }
+
+// // add new Node
+// linkedList.prototype.addItem = function (value){
+//    let newNode = new Node(value);
+//    if(this.head === null){
+//     this.head = newNode;
+//    }else{
+//     let currentNode = this.head;
+//     while(currentNode.next !== null){
+//         currentNode = currentNode.next;
+//     }
+//     currentNode.next = newNode;
+//    }
+//    this.length++;
+// }
+
+// // delete node by value
+// linkedList.prototype.deleteNodeByValue = function (value){
+//     if(this.head === null) return null;
+
+//     // handle head
+//     if(this.head.value === value){
+//         this.head = this.head.next;
+//         this.length--;
+//         return;
+//     }
+
+//     let prev = null;
+//     let currentNode = this.head;
+//     while(currentNode !== null && currentNode.value !== value){
+//         prev = currentNode;
+//         currentNode = currentNode.next;
+//     }
+
+//     if(currentNode === null) return null;
+
+//     prev.next = currentNode.next;
+
+//     this.length--;
+// }
+
+// // Insert after value
+// linkedList.prototype.insertAfterValue = function (existingValue, newValue) {
+//     if (this.head === null) return null;
+
+//     let newNode = new Node(newValue);
+//     let currentNode = this.head;
+
+//     while (currentNode !== null && currentNode.value !== existingValue) {
+//         currentNode = currentNode.next;
+//     }
+
+//     if (currentNode === null) return null; // value not found
+
+//     let nextNode = currentNode.next;
+//     currentNode.next = newNode;
+//     newNode.next = nextNode;
+
+//     this.length++;
+// };
+
+// // find second last Node 
+// linkedList.prototype.findSecondLastNode = function () {
+//     if (this.head === null || this.head.next === null) {
+//         return null;
+//     }
+
+//     let prev = null;
+//     let currentNode = this.head;
+
+//     while (currentNode.next !== null) {
+//         prev = currentNode;
+//         currentNode = currentNode.next;
+//     }
+
+//     return prev.value;
+// };
+
+// // insert before value
+// linkedList.prototype.insertBeforeValue = function (existingValue, newValue){
+//     if(this.head === null) return null;
+
+//     let newNode = new Node(newValue);
+//     // if existing value at head
+//     if(this.head.value === existingValue){
+//         let nextNode = this.head;
+//         this.head = newNode;
+//         newNode.next = nextNode;
+//         this.length++;
+//         return;
+//     }
+
+//     let prev = null;
+//     let currentNode = this.head;
+//     while(currentNode !== null && currentNode.value !== existingValue){
+//         prev = currentNode;
+//         currentNode = currentNode.next;
+//     }
+
+//     if(currentNode === null) return null;
+//     prev.next = newNode;
+//     newNode.next = currentNode;
+//     this.length++;
+// }
+
+
+
+// // Traverse the linked list
+// linkedList.prototype.travers = function (value){
+//    if(this.head === null){
+//     return null;
+//    }else{
+//     let currentNode = this.head;
+//     while(currentNode !== null){
+//         console.log(currentNode.value);
+//         currentNode = currentNode.next;
+//     }
+//    }
+// }
+
+// let list = new linkedList();
+// list.addItem(10);
+// list.addItem(20);
+// list.addItem(30);
+// list.addItem(40);
+// list.addItem(50);
+// list.addItem(60);
+// list.addItem(70);
+// list.addItem(80);
+// list.addItem(90);
+// list.addItem(100);
+// // Delete node by value
+// // list.deleteNodeByValue(70);
+// // Insert after value
+// // list.insertAfterValue(60,70)
+// // Find 2nd last node
+// // console.log("Second last Node value :",list.findSecondLastNode());
+// // Insert before value
+// // list.insertBeforeValue(existingValue, newValue)
+
+// list.travers();
+
+
+
+
+// NEW DAY...................................
+
+// create new node
+function Node(value, next = null){
     this.value = value;
     this.next = next;
 }
 
-// create linkedList
+// Create linkedList
 function linkedList (){
     this.head = null;
     this.length = 0;
 }
 
-// add new Node
 linkedList.prototype.addItem = function (value){
-   let newNode = new Node(value);
-   if(this.head === null){
-    this.head = newNode;
-   }else{
-    let currentNode = this.head;
-    while(currentNode.next !== null){
-        currentNode = currentNode.next;
+    let newNode = new Node(value);
+    if(this.head === null){
+        this.head = newNode;
+    }else {
+        let currentNode = this.head;
+        while(currentNode.next !== null){
+            currentNode = currentNode.next;
+        }
+        currentNode.next = newNode;
     }
-    currentNode.next = newNode;
-   }
-   this.length++;
-}
-
-// delete node by value
-linkedList.prototype.deleteNodeByValue = function (value){
-    if(this.head === null) return null;
-
-    // handle head
-    if(this.head.value === value){
-        this.head = this.head.next;
-        this.length--;
-        return;
-    }
-
-    let prev = null;
-    let currentNode = this.head;
-    while(currentNode !== null && currentNode.value !== value){
-        prev = currentNode;
-        currentNode = currentNode.next;
-    }
-
-    if(currentNode === null) return null;
-
-    prev.next = currentNode.next;
-
-    this.length--;
-}
-
-// Insert after value
-linkedList.prototype.insertAfterValue = function (existingValue, newValue) {
-    if (this.head === null) return null;
-
-    let newNode = new Node(newValue);
-    let currentNode = this.head;
-
-    while (currentNode !== null && currentNode.value !== existingValue) {
-        currentNode = currentNode.next;
-    }
-
-    if (currentNode === null) return null; // value not found
-
-    let nextNode = currentNode.next;
-    currentNode.next = newNode;
-    newNode.next = nextNode;
-
     this.length++;
-};
+}
 
-// find second last Node 
-linkedList.prototype.findSecondLastNode = function () {
-    if (this.head === null || this.head.next === null) {
+linkedList.prototype.travese = function (){
+    if(this.head === null){
+        return null;
+    }else{
+        let currentNode = this.head;
+        while(currentNode !== null){
+            console.log(currentNode.value);
+            currentNode = currentNode.next;
+        }
+    }
+}
+// simple technique for findingNthFromEnd
+linkedList.prototype.findNthFromEnd = function (n) {
+    if (this.head === null || n > this.length) {
         return null;
     }
 
-    let prev = null;
+    let index = 0;
     let currentNode = this.head;
 
-    while (currentNode.next !== null) {
-        prev = currentNode;
+    while (index < (this.length - n)) {
         currentNode = currentNode.next;
+        index++;
     }
 
-    return prev.value;
+    return currentNode.value;
+}
+
+// Two pointer technique for finding nth from last
+linkedList.prototype.findNthFromEnd = function (n) {
+    if (this.head === null) return null;
+
+    let a = this.head;
+    let b = this.head;
+
+    // move pointer b n steps ahead
+    for (let i = 0; i < n; i++) {  // very Important this is how we move pointer on node
+        if (b === null) return null; // n > length
+        b = b.next;
+    }
+
+    // move both pointers together
+    while (b !== null) {
+        a = a.next;
+        b = b.next;
+    }
+
+    return a.value;
 };
 
-// insert before value
-linkedList.prototype.insertBeforeValue = function (existingValue, newValue){
-    if(this.head === null) return null;
 
-    let newNode = new Node(newValue);
-    // if existing value at head
-    if(this.head.value === existingValue){
-        let nextNode = this.head;
-        this.head = newNode;
-        newNode.next = nextNode;
-        this.length++;
-        return;
-    }
-
-    let prev = null;
-    let currentNode = this.head;
-    while(currentNode !== null && currentNode.value !== existingValue){
-        prev = currentNode;
-        currentNode = currentNode.next;
-    }
-
-    if(currentNode === null) return null;
-    prev.next = newNode;
-    newNode.next = currentNode;
-    this.length++;
-}
-
-
-
-// Traverse the linked list
-linkedList.prototype.travers = function (value){
-   if(this.head === null){
-    return null;
-   }else{
-    let currentNode = this.head;
-    while(currentNode !== null){
-        console.log(currentNode.value);
-        currentNode = currentNode.next;
-    }
-   }
-}
 
 let list = new linkedList();
 list.addItem(10);
@@ -142,13 +247,5 @@ list.addItem(70);
 list.addItem(80);
 list.addItem(90);
 list.addItem(100);
-// Delete node by value
-// list.deleteNodeByValue(70);
-// Insert after value
-// list.insertAfterValue(60,70)
-// Find 2nd last node
-// console.log("Second last Node value :",list.findSecondLastNode());
-// Insert before value
-list.insertBeforeValue(existingValue, newValue)
-
-list.travers();
+console.log("Answer is :",list.findNthFromEnd(3));
+list.travese()
