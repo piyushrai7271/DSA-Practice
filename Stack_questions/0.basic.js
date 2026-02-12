6aa50f8352d64972dcfe98ddd5ae10bb3672b01f
piyush rai <piyushrai7271@gmail.com>
2026-02-12 14:50:23 +0530

    stack interview question

--- Stack_questions/0.basic.js
+++ Stack_questions/0.basic.js
@@ -33,8 +33,6 @@
 //   return this.items.length === 0;
 // };
 
-
-
 // const stack = new Stack();
 
 // stack.push(10);
@@ -47,29 +45,85 @@
 // linked list with stack
 
 // create node
-function Node (value, next = null){
-    this.value = value;
-    this.next = next;
+// function Node (value, next = null){
+//     this.value = value;
+//     this.next = next;
+// }
+
+// // create linkedList
+// function linkedList (){
+//     this.head = null;
+//     this.length = 0;
+// }
+
+// // push for stack
+// linkedList.prototype.push = function (value){
+//     let newNode = new Node(value);
+//     if(this.head === null){
+//         this.head = newNode;
+//     }else{
+//         let currentNode = this.head;
+//         let nextNode = newNode
+//     }
+// }
+
+// let list = new linkedList();
+// list.push(value);
+// list.pop();
+
+// New Day...........
+
+function Stack() {
+  this.items = [];
 }
 
-// create linkedList
-function linkedList (){
-    this.head = null;
-    this.length = 0;
-}
+Stack.prototype.push = function (elem) {
+  return (this.items[this.items.length] = elem);
+};
 
-// push for stack
-linkedList.prototype.push = function (value){
-    let newNode = new Node(value);
-    if(this.head === null){
-        this.head = newNode;
-    }else{
-        let currentNode = this.head;
-        let nextNode = newNode
+Stack.prototype.pop = function () {
+  if (this.isEmpty()) {
+    return "Stack is Empty.";
+  }
+
+  return this.items.pop();
+};
+
+Stack.prototype.peek = function () {
+  if (this.isEmpty()) {
+    return "Stack is Empty";
+  }
+
+  return this.items[this.size() - 1];
+};
+
+Stack.prototype.isEmpty = function () {
+  return this.size() === 0;
+};
+
+Stack.prototype.size = function () {
+  return this.items.length;
+};
+
+Stack.prototype.travers = function(){
+    if(this.isEmpty()){
+        console.log("Stack is Empty ");
+        return;
+    }
+
+    for(let i=0; i < this.items.length ; i++){
+        console.log(this.items[i]);
     }
 }
 
-
-let list = new linkedList();
-list.push(value);
-list.pop();
+let stack = new Stack();
+stack.push(10);
+stack.push(20);
+stack.push(30);
+stack.push(40);
+stack.push(50);
+console.log("Last Element :",stack.pop());
+console.log("Is stack Empty :",stack.isEmpty());
+console.log("Last element :",stack.peek());
+console.log("Size of the stack :",stack.size());
+stack.travers();
