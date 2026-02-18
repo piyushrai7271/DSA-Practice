4790c087580821a338d78694a2a6bdbcada46375
piyush rai <piyushrai7271@gmail.com>
2026-02-18 14:17:25 +0530

    stack question

--- fundamental/0.basic_Array.js
+++ fundamental/0.basic_Array.js
@@ -50,12 +50,99 @@
 
 // let x = 123;
 // var isPalindrome = function(x) {
-//     for(let i=0,j=x.length-1; i<j; i++,j--){
-//         if(x[i] !== x[j]){
-//             return false;
-//         }
-//     }
-//     return true;
+    
 // };
 
-// console.log(isPalindrome(x));
+// let num = 123
+// let first = num%10; // giving last number 
+// let firstDivid = Math.floor(num/10);  // giving remaning number
+// console.log(first);
+// console.log(firstDivid);
+// let second = firstDivid%10;
+// let secondDivid = Math.floor(firstDivid/10);
+// console.log(second);
+// console.log(secondDivid);
+
+// let num = 12345;
+
+// function printRevers (num) {
+
+//     while(num > 0){
+//         console.log(num%10);
+//         num = Math.floor(num/10);
+//     }
+// }
+
+// printRevers(num);
+
+
+// let num = 12345;
+
+// function printRevers (num) {
+//     let count = 0;
+//     while(num > 0){
+//         // console.log(num%10);
+//         count++;
+//         num = Math.floor(num/10);
+//     }
+//     return count;
+// }
+
+// console.log("Count of  number :",printRevers(num));
+
+// let num = 12345;
+
+// function printRevers (num) {
+//     let sum = 0;
+//     while(num > 0){
+//         let lastNum = num%10;
+//         sum = sum + lastNum;
+//         num = Math.floor(num/10);
+//     }
+//     return sum;
+// }
+
+// console.log("Sum of  number :",printRevers(num));
+
+// let num = -12345;
+
+// function reverseNumber(num) {
+//     let sign = num < 0 ? -1 : 1;
+//     num = Math.abs(num);
+
+//     let reNumber = 0;
+
+//     while (num > 0) {
+//         let lastNum = num % 10;
+//         reNumber = reNumber * 10 + lastNum;
+//         num = Math.floor(num / 10);
+//     }
+
+//     return reNumber * sign;
+// }
+
+// console.log("Reverse of number:", reverseNumber(num));
+
+// check the number is palindrom 
+
+// function isPalindrome(num) {
+//     let original = num;
+//     let reversed = 0;
+
+//     while (num > 0) {
+//         let digit = num % 10;
+//         reversed = reversed * 10 + digit;
+//         num = Math.floor(num / 10);
+//     }
+
+//     return original === reversed;
+// }
+
+// console.log(isPalindrome(121)); // true
+// console.log(isPalindrome(123)); // false
+
+
+
+
+
+
