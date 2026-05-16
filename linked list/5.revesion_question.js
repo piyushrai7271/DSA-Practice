2d9a9a47741ee82a0dfa89593bd7d80a6b18bfc1
piyush rai <piyushrai7271@gmail.com>
2026-05-16 16:38:31 +0530

    sliding window on dtring based question

--- linked list/5.revesion_question.js
+++ linked list/5.revesion_question.js
@@ -14,11 +14,10 @@
 // 10- update tail node
 
 // test start...........................................................
-
-// create Node
-function Node(value,next=null){
-    this.value = value;
-    this.next = next;
+// create node 
+function Node (value, next=null){
+   this.value = value;
+   this.next = next;
 }
 
 // create linked list
@@ -27,16 +26,9 @@ function linkedList (){
     this.length = 0;
 }
 
-// add new Item 
+// add new node
 linkedList.prototype.addItem = function (value){
     let newNode = new Node(value);
-
-    // if linked list is empty
-    if(this.head === null){
-        this.head = newNode;
-        this.length++;
-        return ;
-    }
 
     let currentNode = this.head;
     while(currentNode.next !== null){
@@ -44,37 +36,16 @@ linkedList.prototype.addItem = function (value){
     }
     currentNode.next = newNode;
     this.length++;
-    return;
 }
 
-// add item at head
-linkedList.prototype.addItemAtHead = function (value){
-    let newNode = new Node(value);
+// add new node at head
+linkedList.prototype.addItemAtHead = function (value){}
 
-    let headNode = this.head;
-    this.head = newNode;
-    newNode.next = headNode;
-    this.length++;
-    return ;
-}
+// add new node at given Index
+linkedList.prototype.addItemAtIndex = function (value, index){}
 
-// add item at given Index
-linkedList.prototype.addItemAtIndex = function (index,value){
-    if(index < 0 || index > this.length) return null;
-
-    let newNode = 
-}
-
-// delete at head item
-linkedList.prototype.deleteHeadItem = function (){}
-
-// delete at tail item
-linkedList.prototype.deleteTailItem = function (){}
-
-// travers the linked list
+// travers with linked list
 linkedList.prototype.travers = function (){
-    if(this.head === null) return null;
-
     let currentNode = this.head;
     while(currentNode !== null){
         console.log(currentNode.value);
@@ -83,13 +54,10 @@ linkedList.prototype.travers = function (){
 }
 
 
-let list = new linkedList();
+let list = new linkedList()
 list.addItem(10);
 list.addItem(20);
 list.addItem(30);
 list.addItem(40);
-list.addItemAtHead(0);
-list.addItemAtIndex(index , value);
-list.deleteHeadItem();
-list.deleteTailItem();
+list.addItemAtHead(0)
 list.travers();
